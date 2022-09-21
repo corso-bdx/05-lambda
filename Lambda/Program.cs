d31085c60016aa8bc39cd6b20fc218b775dac01b
Fabio Iotti <fabio.iotti@alad.cloud>
2022-09-21 14:21:41 +0200

    Parametri, out, ref

--- Lambda/Program.cs
+++ Lambda/Program.cs
@@ -57,3 +57,37 @@ StampaEspressione(1, "+", 2);
 StampaEspressione(5, "-", 3);
 StampaEspressione(10, "*", 5);
 StampaEspressione(20, "/", 4);
+
+
+#region Parametri in out ed in ref
+
+void Metodo1(string parametro) {
+    Console.WriteLine($"Parametro: {parametro}");
+}
+
+Metodo1("PARAMETRO");
+
+
+
+string Metodo2(out string parametroOut) {
+    parametroOut = "OUT";
+    return "RETURN";
+}
+
+string o;
+string r = Metodo2(out o);
+Console.WriteLine($"r: {r}");
+Console.WriteLine($"o: {o}");
+
+
+
+void Metodo3(ref string parametroRef) {
+    Console.WriteLine($"Parametro ref: {parametroRef}");
+    parametroRef = "REF_2";
+}
+
+string v = "REF_1";
+Metodo3(ref v);
+Console.WriteLine($"v: {v}");
+
+#endregion
